34f97b4c1880d834e7c688962af720238db89f2d
Alexander Salinas <alexanderr-salinas@hotmail.com>
2022-03-31 12:05:09 -0300

    segunda clase

--- script/script.js
+++ script/script.js
@@ -1,7 +1,25 @@
+/*CLASE 1:
+
 let numero1 = 1;
 let numero2 = 4;
 let numero3 = 7;
 let entrada = parseFloat (prompt("Ingresa un numero"));
 let salida = entrada + numero1 * numero3 /numero2;
 alert (salida);
+
+*/
+
+/*CLASE 2:*/
+
+let nombre = prompt("cual es tu nombre");
+
+if (nombre == "Rodrigo"){
+    alert("Sos el profesor");
+}
+else if ((nombre != "") && ((nombre === "Mauricio") || (nombre === "Samuel") || (nombre === "Juan"))){
+    alert("Sos un tutor");
+}
+else{
+    alert("Sos un alumno");
+}
 
